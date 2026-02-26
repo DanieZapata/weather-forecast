3d9b7f6fca593bb260df480cd40f1702434d8e54
DanieZapata <zapataparidaniela@gmail.com>
2026-02-25 21:30:36 -0400

    style: weather information and messages

--- src/components/CurrentWeather.tsx
+++ src/components/CurrentWeather.tsx
@@ -14,19 +14,26 @@ export function CurrentWeather({ city, data, todayForecast, weatherInfo }: Props
   const formattedDate = new Intl.DateTimeFormat("es-ES", { day: "2-digit" }).format(date);
 
   return (
-    <div className="mt-4 py-4 mx-auto flex flex-col md:flex-row justify-between items-center md:items-start gap-4">
+    <div className="mt-4 py-4 mx-auto flex flex-col md:flex-row justify-between items-center md:items-start gap-8">
 
-      <div className="flex flex-col items-center md:items-start text-center md:text-left">
-        <h2 className="uppercase text-6xl font-thin text-white">{city}</h2>
-        <p className="uppercase text-2xl font-thin text-white">{`${formattedDay} ${formattedDate}`}</p>
-        <p className="uppercase mt-2 font-thin text-2xl text-white">{weatherInfo.description}</p>
+      <div className="flex flex-col md:flex-row justify-between items-start gap-4">
+        <div className="flex flex-col gap-2">
+          <div className="flex flex-col">
+            <h2 className="uppercase text-6xl font-thin text-white">{city}</h2>
+            <p className="uppercase text-2xl font-thin text-white">{`${formattedDay} ${formattedDate}`}</p>
+            <p className="uppercase mt-2 font-thin text-2xl text-white">{weatherInfo.description}</p>
+          </div>
+
+          <div className="flex flex-col md:flex-row md:gap-6 mt-2">
+            <p className="text-lg font-thin text-white">
+              Temperatura: <span className="font-semibold text-white">{data.temperature}°C</span>
+            </p>
+            <p className="text-lg font-thin text-white">
+              Viento: <span className="font-semibold text-white">{data.windspeed} km/h</span>
+            </p>
+          </div>
+        </div>
       </div>
-
-      <div className="flex flex-col items-center md:items-end text-center md:text-right gap-1">
-        <p className="text-lg font-thin text-white">Temperatura: <span className="font-semibold text-white">{data.temperature}°C</span></p>
-        <p className="text-lg font-thin text-white">Viento: <span className="font-semibold text-white">{data.windspeed} km/h</span></p>
-      </div>
-
     </div>
   );
 }
--- src/components/SearchCity.tsx
+++ src/components/SearchCity.tsx
@@ -11,13 +11,13 @@ export function SearchCity({ onSearch }: Props) {
         <div>
             <div className="flex mx-auto gap-2">
                 <input
-                    className="flex-1 border p-2 rounded-lg"
+                    className="flex-1 rounded-lg bg-gray-950 bg-opacity-50 p-1 text-white placeholder:text-white-400 focus:outline-none"
                     placeholder="Buscar ciudad"
                     value={value}
                     onChange={(e) => setValue(e.target.value)}
                 />
                 <button
-                    className="bg-blue-950 text-white px-6 py-2 rounded-xl"
+                    className="bg-gray-900 text-white px-6 py-2 rounded-xl"
                     onClick={() => onSearch(value)}
                 >
                     Buscar
